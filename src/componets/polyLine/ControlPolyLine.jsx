df7b4bc0b5bd4c129aba271fd702813599a8758c
Miguel-G2 <mposada@gupodos.com.mx>
2024-05-21 17:59:27 -0600

    Parametro vías -- por completar

--- src/componets/polyLine/ControlPolyLine.jsx
+++ src/componets/polyLine/ControlPolyLine.jsx
@@ -1,12 +1,13 @@
 import { LayerGroup, LayersControl, Polyline } from "react-leaflet"
 import { dataVias } from "../../assets/data/dataVias"
+import { dataVias2 } from "../../assets/data/dataVias2"
 
 export const ControlPolyLine = () => {
   return (
     <>
         <LayersControl.Overlay checked name="Vías">
             <LayerGroup>
-                <Polyline pathOptions={{color:'purple'}} positions={dataVias}/>
+                <Polyline pathOptions={{color:'purple'}} positions={dataVias2}/>
             </LayerGroup>
         </LayersControl.Overlay>
     </>
