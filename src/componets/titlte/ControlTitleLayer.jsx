b5a73b2bbe6e5130fd68255832a38cb6479c38b8
Miguel-G2 <mposada@gupodos.com.mx>
2024-05-27 10:05:08 -0600

    Mapa Satelital prueba

--- src/componets/titlte/ControlTitleLayer.jsx
+++ src/componets/titlte/ControlTitleLayer.jsx
@@ -11,7 +11,7 @@ export const ControlTitleLayer = () => {
             </LayersControl.BaseLayer>
             <LayersControl.BaseLayer name="Satellite">
                 <TileLayer
-                    url='https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}'
+                    url='https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}?api_key=84e0a838-3ec8-441e-9e16-e8dd22f42276'
                     minZoom={0}
                     maxZoom={20}
                     attribution='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
